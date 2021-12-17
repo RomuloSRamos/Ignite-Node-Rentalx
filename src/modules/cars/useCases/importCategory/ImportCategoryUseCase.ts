10df42774bc3d314247ddcb849feb0f1da04317b
Romulo S. Ramos <rsromulo@msn.com>
2021-12-16 21:51:29 -0300

    Inserindo os dados do upload no repositório

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -33,7 +33,13 @@ class ImportCategoryUseCase {
   }
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log("execute...categories:", categories);
+    categories.map(async (category) => {
+      const { name, description } = category;
+      const existCategory = this.categoriesRepository.findByName(name);
+      if (!existCategory) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
   }
 }
 export { ImportCategoryUseCase };
