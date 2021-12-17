8ea4ec901cdbff86ac9eab3143a0690ae9b5a253
Romulo S. Ramos <rsromulo@msn.com>
2021-12-16 21:10:22 -0300

    ImportCategory

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,14 +1,30 @@
 import { parse } from "csv-parse";
 import fs from "fs";
 
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+  loadCategories(file: Express.Multer.File): IImportCategory[] {
     const stream = fs.createReadStream(file.path);
+    const categories: IImportCategory[] = [];
     const parseFile = parse({});
     stream.pipe(parseFile);
     parseFile.on("data", async (line) => {
-      console.log(line);
+      const [name, description] = line;
+      categories.push({ name, description });
+      console.log("loadCategories...categories:", categories);
     });
+    return categories;
+  }
+  execute(file: Express.Multer.File): void {
+    const categories = this.loadCategories(file);
+    console.log("execute...categories:", categories);
   }
 }
 export { ImportCategoryUseCase };
