995461d6141c6e4c547323c7d7aa9622b9d11357
Romulo S. Ramos <rsromulo@msn.com>
2022-01-11 21:28:15 -0300

    Chapter IV - Criando seed de usuário

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,14 +3,14 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
+import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
-import "../typeorm";
-import "../../container";
-
+createConnection();
 const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
